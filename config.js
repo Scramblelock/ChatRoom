8412823cb14d64f178ead270a4b4ac7634547913
Marc Sakalauskas <scrambledlegs39@hotmail.com>
2018-11-02 14:19:55 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/5a3240b7-54e7-40be-83e4-7238e033ccc4/token";
+const instanceLocator = "v1:us1:5a3240b7-54e7-40be-83e4-7238e033ccc4";
+
+export { tokenUrl, instanceLocator }
+
